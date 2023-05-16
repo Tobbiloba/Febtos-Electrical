998bc079e6879ea36d8fb710db8cbf6addc43193
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-05-16 14:56:36 +0100

    started a new project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import HomePage from "./components/Homepage";
+import Menu from "./components/Menu";
+
+function App() {
+  return (
+    <div className="">
+      <Menu />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+const Menu = () => {
+    return (
+        <div className="border container">
+            <h1>Menu</h1>
+        </div>
+    )
+}
+export default Menu;
