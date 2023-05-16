c8ce56799bbc59bb2452370de623c106e2dd6b49
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-05-16 23:59:20 +0100

    added menu open and close in mobile view

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,25 @@
 import { useState } from "react";
+import ElectricMeterIcon from '@mui/icons-material/ElectricMeter';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 const Menu = () => {
+    const [showMenu, setShowMenu] = useState(false)
     return (
-        <div className="border container">
-            <h1>Menu</h1>
+        <div className="border max-w-screen">
+            <div className="flex lg:hidden">
+                <div className={`container flex-row flex justify-between items-center py-2`}>
+                    <div className="flex flex-row">
+                        <ElectricMeterIcon style={{ color: '#f97316', fontSize: '28px' }} />
+                        <h1 className="text-xl ml-1 font-bold text-slate-700">Feb<span className="text-orange-500">Tos</span></h1>
+                    </div>
+                    <div>
+                        {!showMenu && <MenuIcon className={`${showMenu === false ? 'flex' : 'hidden'}`} style={{ color: '#334155', fontSize: '28px' }} onClick={() => setShowMenu(true)} />}
+                        {showMenu && <CloseIcon className={`${showMenu === true ? 'flex' : 'hidden'}`} style={{ color: '#f97316', fontSize: '28px' }} onClick={() => setShowMenu(false)} />}
+
+                    </div>
+                </div>
+                <div></div>
+            </div>
         </div>
     )
 }
