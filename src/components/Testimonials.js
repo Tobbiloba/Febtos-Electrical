6468931aca86beff8ca3300dbd80ab3e90167f99
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-05-25 13:59:11 +0100

    added workers slider

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -10,7 +10,7 @@ import "swiper/css/navigation";
 // import "./styles.css";
 
 // import required modules
-import { Autoplay } from "swiper";
+import { Autoplay, Pagination, Navigation } from "swiper";
 
 const Testimonials = () => {
     const customerReview = [
@@ -44,6 +44,56 @@ const Testimonials = () => {
             review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
         }
     ]
+    const workers = [
+        {
+            id: 1,
+            position: 'Manager',
+            img: 'https://imageio.forbes.com/specials-images/imageserve/515124952/960x0.jpg?format=jpg&width=960',
+            name: 'Sarah Smith'
+        },
+        {
+            id: 2,
+            position: 'Team Leader',
+            img: 'https://www.simplilearn.com/ice9/free_resources_article_thumb/How_to_become_a_marketing_manager.jpg',
+            name: 'Smith White'
+        },
+        {
+            id: 3,
+            position: 'Head of IT',
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdrFiMxL_5NKR6bgtQwzPQk54SOA9rSsHStg&usqp=CAU',
+            name: 'Wihte Larry'
+        },
+        {
+            id: 4,
+            position: 'Senior Engineer',
+            img: 'https://images.westend61.de/0000762684pw/senior-engineer-wearing-safety-helmet-portrait-ZEF11929.jpg',
+            name: 'Larry Jones'
+        },
+        {
+            id: 5,
+            position: 'Electrical Supervisor',
+            img: 'https://st2.depositphotos.com/1091429/10312/i/600/depositphotos_103124140-stock-photo-senior-engineer-in-hardhat.jpg',
+            name: 'Jones Scott'
+        },
+        {
+            id: 6,
+            position: 'Designer',
+            img: 'https://thumbs.dreamstime.com/b/confident-engineer-blue-jumpsuit-holding-laptop-computer-warehouse-confident-engineer-blue-jumpsuit-holding-laptop-183509689.jpg',
+            name: 'Scott Micheal'
+        },
+        {
+            id: 7,
+            position: 'Electrical Engineer',
+            img: 'https://res.cloudinary.com/grand-canyon-university/image/fetch/w_750,h_564,c_fill,g_faces,q_auto/https://www.gcu.edu/sites/default/files/2020-08/mechanical_engineer.jpg',
+            name: 'Mary sandra'
+        },
+        {
+            id: 8,
+            position: 'Mechanical Engineer',
+            img: 'https://images.thewest.com.au/publication/C-5603266/9f40a43dfb5e247fc74ddb2e81c82cf2d2c27496-16x9-x0y88w4000h2250.jpg?imwidth=810&impolicy=wan_v3',
+            name: 'Diya Sanjay'
+        }
+    ]
 
     return (
         <div className="container flex flex-col py-6">
@@ -56,6 +106,7 @@ const Testimonials = () => {
                     <div>
                         <Swiper
                             spaceBetween={30}
+                            // slidesPerView={2}
                             centeredSlides={true}
                             autoplay={{
                                 delay: 2500,
@@ -109,9 +160,45 @@ const Testimonials = () => {
                 </div>
 
             </div>
-            <div>
-                <div></div>
-                <div></div>
+            <div className="mt-8">
+                <div className="">
+                    <h1 className="flex justify-center text-xl font-bold italic">Our Experts</h1>
+                    <h1 className="mt-1 font-bold text-2xl text-center text-slate-800">Team</h1>
+                </div>
+                <div className="mt-4">
+                    <Swiper
+                        spaceBetween={30}
+                        centeredSlides={true}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+                        // pagination={{
+                        //     clickable: true,
+                        // }}
+                        navigation={true}
+                        modules={[Autoplay, Navigation]}
+                        className="mySwiper"
+                    >
+                        {workers.map((item) => {
+                            return (
+                                <SwiperSlide key={item.id}>
+
+                                    <div className="h-[300px] px-[20vw] flex justify-center items-end">
+                                        <img className="absolute top-0 w-full h-[270px]" src={item.img} />
+                                        <div className="z-10 h-[80px] flex flex-col justify-center items-center w-full border bg-white">
+                                            <h1 className="font-bold text-xl font-mono">{item.name}</h1>
+                                            <p>{item.position}</p>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                            )
+                        })}
+
+
+
+                    </Swiper>
+                </div>
             </div>
         </div>
     )
